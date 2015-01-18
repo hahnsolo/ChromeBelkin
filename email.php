fcfee91263489b97ba48fa8022a9bded6ee21392
Chris Hahn <soundsgood@greenseedmusic.com>
2015-01-18 12:44:18 -0500

    Revert "ChromecastWorkingOnClick"
    This reverts commit 481f600272ae2e704e287c8d27970a156d7af79e.

--- email.php
+++
@@ -1,7 +0,0 @@
-<?php
-// the message
-$msg = "You Left The Lights On";
-// use wordwrap() if lines are longer than 70 characters
-// send email
-mail("soundsgood@greenseedmusic.com","Lights",$msg);
-?>
